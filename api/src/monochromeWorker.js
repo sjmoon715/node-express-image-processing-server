c1619358a30ab84d75c9d0a4bc81b59230448932
Stephen Moon <stephenmoon@college.harvard.edu>
2022-07-12 13:35:20 -0400

    Checking Task 3

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -2,8 +2,6 @@ const gm = require('gm');
 const {workerData, parentPort} = require('worker_threads');
 
 gm(workerData.source).monochrome().write(workerData.destination, (error) => {
-    if (typeof error === "boolean"){
-        parentPort.postMessage({monochrome : true});
-        throw(error);
-    }
+    if (error) throw(error);
+    parentPort.postMessage({monochrome : true});
 });
